ced1e0c1356ecad4cbb42f9b189e595227a3f097
Victor <visama396@gmail.com>
2026-03-31 18:36:40 +0200

    Redoing app

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -4,3 +4,14 @@ const supabaseUrl = import.meta.env.PUBLIC_VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.PUBLIC_VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const checkAuth = async () => {
+  const { data: { session } } = await supabase.auth.getSession();
+  return session;
+}
+
+export const getProfile = async (name: string, password: string) => {
+  const fakeEmail = `${name}@app.local`
+  const { data: { user } } = await supabase.auth.signInWithPassword({ email: fakeEmail, password });
+  return user;
+}
